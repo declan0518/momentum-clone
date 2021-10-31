594335928f35ad72e785107715924c5340670b62
Haneul Ji <declan0518@gmail.com>
2021-10-31 10:42:00 +0900

    Random quote

--- js/greeting.js
+++ js/greeting.js
@@ -3,21 +3,22 @@ const loginInput = document.querySelector(".login-form__input");
 const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const handleSubmit = (event) => {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   paintGreeting(username);
 };
 
 const paintGreeting = (username) => {
-  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
 };
 
-const savedUsername = localStorage.getItem("username");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
